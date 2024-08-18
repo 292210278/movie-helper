ba0419d7a0f7bdfb04dd16b5f77e41949dca22f5
292210278 <292210278@qq.com>
2024-08-18 17:10:08 +0800

    第一次修改

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ export const OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmMyYzNhYTZiZTQyZmUyYzNjMmVjMGY3ZTA2NzRkYiIsInN1YiI6IjY1YTkxNTQ2MGU1YWJhMDEyZjdkZWExOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kVUz4IMzkVlHyrDDYYB5sEW92jliyy2lsBvtPJOg2kg",
+    Authorization: "Bearer 填写自己的APIKey",
   },
 };
